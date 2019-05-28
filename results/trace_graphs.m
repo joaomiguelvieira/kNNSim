261424d32439b0a28828dcedccbda903cfff14f5
root <root@kara.joaomiguelvieira.ece.utah.edu>
2019-05-28 15:37:05 -0600

    Updated script to generate the Matlab graphs from results.xlsx

--- results/trace_graphs.m
+++ results/trace_graphs.m
@@ -4,55 +4,71 @@ close all;
 
 %% import data from excel
 % datasets characteristics
-[datasets.params, datasets.names] = xlsread("knn_datasets.xlsx", 1, "A2:E9");
+[datasets.params, datasets.names] = xlsread("results.xlsx", 1, "A2:E9");
 datasets.params(6, :) = [];
 datasets.names(6)     = [];
 datasets.names(3)     = {'Breast Cancer'};
 
 % power values estimated by vivado
-device.static  = 0.159;
+device.static = 0.159;
 ps.dynamic = 1.533;
 pl.dynamic = 2.094 - ps.dynamic;
 
+device.static_pcfg1 = 0.144;
+ps.dynamic_pcfg1 = 1.535;
+pl.dynamic_pcfg1 = 1.700 - ps.dynamic_pcfg1;
+
 % timings spent by accelerators
 accel.freq         = 100E6;
-accel.cores        = xlsread("knn_datasets.xlsx", 4, "A1:G1");
+accel.cores        = xlsread("results.xlsx", 4, "A1:G1");
 accel.time         = zeros(7, length(accel.cores));
-accel.time(1:5, :) = xlsread("knn_datasets.xlsx", 4, "A2:G6");
-accel.time(6:7, :) = xlsread("knn_datasets.xlsx", 4, "A8:G9");
+accel.time(1:5, :) = xlsread("results.xlsx", 4, "A2:G6");
+accel.time(6:7, :) = xlsread("results.xlsx", 4, "A8:G9");
 accel.time         = accel.time ./ 1E6;
+
+accel_pcfg1.cores        = xlsread("results.xlsx", 5, "B1:E1");
+accel_pcfg1.time         = zeros(7, length(accel_pcfg1.cores));
+accel_pcfg1.time(1:5, :) = xlsread("results.xlsx", 5, "B2:E6");
+accel_pcfg1.time(6:7, :) = xlsread("results.xlsx", 5, "B8:E9");
+accel_pcfg1.time         = accel_pcfg1.time ./ 1E6;
 
 % processor times
 processor.time      = zeros(7,1);
-processor.time(1:5) = xlsread("knn_datasets.xlsx", 5, "B2:B6");
-processor.time(6:7) = xlsread("knn_datasets.xlsx", 5, "B8:B9");
+processor.time(1:5) = xlsread("results.xlsx", 6, "B2:B6");
+processor.time(6:7) = xlsread("results.xlsx", 6, "B8:B9");
 
 % another case study
-zynq.var_knn.num        = xlsread("knn_datasets.xlsx", 8, "B1:I1");
-zynq.var_knn.luts       = xlsread("knn_datasets.xlsx", 8, "B5:I5");
-zynq.var_knn.luts_max   = xlsread("knn_datasets.xlsx", 8, "J5");
-zynq.var_knn.accel_time = xlsread("knn_datasets.xlsx", 8, "B2:I2") / 1E6;
-zynq.var_knn.arm_time   = xlsread("knn_datasets.xlsx", 8, "B3:I3");
+zynq.var_knn.num        = xlsread("results.xlsx", 9, "B1:I1");
+zynq.var_knn.luts       = xlsread("results.xlsx", 9, "B5:I5");
+zynq.var_knn.luts_max   = xlsread("results.xlsx", 9, "J5");
+zynq.var_knn.accel_time = xlsread("results.xlsx", 9, "B2:I2") / 1E6;
+zynq.var_knn.arm_time   = xlsread("results.xlsx", 9, "B3:I3");
 
-zynq.var_training.num        = xlsread("knn_datasets.xlsx", 9, "B1:N1");
-zynq.var_training.accel_time = xlsread("knn_datasets.xlsx", 9, "B2:N2") / 1E6;
-zynq.var_training.arm_time   = xlsread("knn_datasets.xlsx", 9, "B3:N3");
+zynq.var_training.num        = xlsread("results.xlsx", 10, "B1:N1");
+zynq.var_training.accel_time = xlsread("results.xlsx", 10, "B2:N2") / 1E6;
+zynq.var_training.arm_time   = xlsread("results.xlsx", 10, "B3:N3");
 
-zynq.var_features.num        = xlsread("knn_datasets.xlsx", 10, "B1:K1");
-zynq.var_features.accel_time = xlsread("knn_datasets.xlsx", 10, "B2:K2") / 1E6;
-zynq.var_features.arm_time   = xlsread("knn_datasets.xlsx", 10, "B3:K3");
+zynq.var_features.num        = xlsread("results.xlsx", 11, "B1:K1");
+zynq.var_features.accel_time = xlsread("results.xlsx", 11, "B2:K2") / 1E6;
+zynq.var_features.arm_time   = xlsread("results.xlsx", 11, "B3:K3");
 
 %% calculate results
 % performance variation with number of cores
-results.var_accel = [accel.cores; accel.time(end, :)];
+results.var_accel = [accel.cores; accel.time(1, :); accel.time(end, :)];
+results_pcfg1.var_accel = [accel_pcfg1.cores; accel_pcfg1.time(1, :); accel_pcfg1.time(end, :)];
 
 % performance improvements
 results.performance = processor.time ./ accel.time;
+results_pcfg1.performance = processor.time ./ accel_pcfg1.time;
 
 % energy improvements
 accel.energy     = accel.time * (device.static + pl.dynamic + ps.dynamic);
 processor.energy = processor.time * (device.static + ps.dynamic);
 results.energy   = processor.energy ./ accel.energy;
+
+accel_pcfg1.energy     = accel_pcfg1.time * (device.static_pcfg1 + pl.dynamic_pcfg1 + ps.dynamic_pcfg1);
+processor_pcfg1.energy = processor.time * (device.static_pcfg1 + ps.dynamic_pcfg1);
+results_pcfg1.energy   = processor_pcfg1.energy ./ accel_pcfg1.energy;
 
 %% plot results
 % generate legend for number of cores
@@ -65,12 +81,11 @@ for i=1:length(accel.cores)
 end
 
 % accelerator execution time
-figure();
-%subplot(1,2,1);
+figure('Position', [699, 259, 560, 300]);
 bar(log2(accel.time * 16384));
 legend(results.legend_cores, "Location", "northwest");
 xticklabels(datasets.names);
-set(gca,'XTickLabelRotation',45);
+set(gca,'XTickLabelRotation',20);
 xlabel("Data Set");
 ylabel("Execution time [s]");
 ax = gca;
@@ -83,20 +98,18 @@ yticklabels({"2^{-14}", "2^{-10}", "2^{-6}", "2^{-2}", "2^{2}", "2^{6}", "2^{10}
 set(gca, 'YGrid', 'on', 'XGrid', 'off', 'YMinorGrid', 'on', 'XMinorGrid', 'off');
 
 % performance improvements
-figure();
-%subplot(1,2,2);
+figure('Position', [699, 259, 560, 300]);
 bar(results.performance);
 legend(results.legend_cores, "Location", "northwest");
 xticklabels(datasets.names);
-set(gca,'XTickLabelRotation',45);
+set(gca,'XTickLabelRotation',20);
 xlabel("Data Set");
 ylabel("Speedup");
 grid on;
 set(gca, 'YGrid', 'on', 'XGrid', 'off', 'YMinorGrid', 'on', 'XMinorGrid', 'off');
 
 % energy improvements
-figure();
-subplot(1,2,2);
+figure('Position', [699, 259, 280, 310]);
 bar(results.energy(:, end));
 xticklabels(datasets.names);
 set(gca,'XTickLabelRotation',45);
@@ -105,8 +118,7 @@ ylabel("Energy effiency improvement");
 set(gca, 'YGrid', 'on', 'XGrid', 'off', 'YMinorGrid', 'on', 'XMinorGrid', 'off');
 
 % accelerator energy consumption
-%figure();
-subplot(1,2,1);
+figure('Position', [699, 259, 280, 310]);
 bar(log2(accel.energy(:, end) * 8192));
 xticklabels(datasets.names);
 set(gca,'XTickLabelRotation',45);
@@ -122,13 +134,27 @@ yticklabels({"2^{-13}", "2^{-7}", "2^{-5}", "2^{-1}", "2^{3}", "2^{7}", "2^{11}"
 set(gca, 'YGrid', 'on', 'XGrid', 'off', 'YMinorGrid', 'on', 'XMinorGrid', 'off');
 
 % performance variation with number of cores
-figure();
-subplot(1,2,1);
+figure('Position', [699, 259, 280, 250]);
+stem(log2(results.var_accel(1, :)), results.var_accel(3, :), "filled");
+xlim([log2(results.var_accel(1, 1)), log2(results.var_accel(1, end))]);
+xticks(log2(results.var_accel(1, :)));
+xticklabels({"1", "2", "4", "8", "16", "24"});
+ylabel("Execution time [s]");
+xlabel("Number of accelerators");
+grid on;
+ax = gca;
+ax.XAxis.MinorTickValues = log2(0:1:24);
+grid minor;
+hold on;
+plot(log2(results.var_accel(1, :)), results.var_accel(3, 1) ./ results.var_accel(1, :), '--');
+legend("Measured", "Theoretical");
+
+% performance variation with number of cores (small dataset)
+figure('Position', [699, 259, 280, 250]);
 stem(log2(results.var_accel(1, :)), results.var_accel(2, :), "filled");
 xlim([log2(results.var_accel(1, 1)), log2(results.var_accel(1, end))]);
 xticks(log2(results.var_accel(1, :)));
 xticklabels({"1", "2", "4", "8", "16", "24"});
-%xticklabels({"2^0", "2^1", "2^2", "2^3", "2^4", "2^4+2^3"});
 ylabel("Execution time [s]");
 xlabel("Number of accelerators");
 grid on;
@@ -140,8 +166,7 @@ plot(log2(results.var_accel(1, :)), results.var_accel(2, 1) ./ results.var_accel
 legend("Measured", "Theoretical");
 
 % speedup versus k
-%figure();
-subplot(1,2,2);
+figure('Position', [699, 259, 370, 250]);
 yyaxis left;
 plot(log2(zynq.var_knn.num), zynq.var_knn.arm_time ./ zynq.var_knn.accel_time, 'o-');
 ylabel("Speedup");
@@ -150,6 +175,7 @@ yyaxis right;
 plot(log2(zynq.var_knn.num), zynq.var_knn.accel_time * 1E3, 'x-');
 ylabel("Execution time [ms]");
 ylim([0, 7]);
+xlim([0, 7]);
 xticks(log2(zynq.var_knn.num));
 xticklabels({"2^0", "2^1", "2^2", "2^3", "2^4", "2^5", "2^6", "2^7"});
 grid on;
@@ -157,7 +183,7 @@ grid minor;
 xlabel("K-Nearest Neighbors");
 
 % luts versus k
-figure();
+figure('Position', [699, 259, 560, 300]);
 bar(zynq.var_knn.luts);
 t=text(1:length(zynq.var_knn.luts),zynq.var_knn.luts,num2str(zynq.var_knn.luts'),'vert','bottom','horiz','center');
 box off;
@@ -167,8 +193,7 @@ ylabel("LUTs");
 ylim([0, 14E3]);
 
 % speedup versus training
-figure();
-subplot(1,2,1);
+figure('Position', [699, 259, 370, 250]);
 yyaxis left;
 plot(log2(zynq.var_training.num), zynq.var_training.arm_time ./ zynq.var_training.accel_time, 'o-');
 ylabel("Speedup");
@@ -187,8 +212,7 @@ xlabel("Number of training samples");
 xlim([2,14]);
 
 % speedup versus features
-%figure();
-subplot(1,2,2);
+figure('Position', [699, 259, 370, 250]);
 yyaxis left;
 plot(log2(zynq.var_features.num), zynq.var_features.arm_time ./ zynq.var_features.accel_time, 'o-');
 ylabel("Speedup");
@@ -205,3 +229,110 @@ grid on;
 grid minor;
 xlabel("Number of features");
 xlim([0,9]);
+
+%% plot results for pcfg1
+% generate legend for number of cores (pcfg1)
+for i=1:length(accel_pcfg1.cores)
+    if accel_pcfg1.cores(i) == 1
+        results_pcfg1.legend_cores(i) = sprintf("%d core", accel_pcfg1.cores(i));
+    else
+        results_pcfg1.legend_cores(i) = sprintf("%d cores", accel_pcfg1.cores(i));
+    end
+end
+
+% accelerator execution time
+figure('Position', [699, 259, 560, 300]);
+bar(log2(accel_pcfg1.time * 16384));
+legend(results_pcfg1.legend_cores, "Location", "northwest");
+xticklabels(datasets.names);
+set(gca,'XTickLabelRotation',20);
+xlabel("Data Set");
+ylabel("Execution time [s]");
+ax = gca;
+ax.YAxis.MinorTickValues = 1:1:28;
+grid on;
+grid minor;
+ylim([0, 28]);
+yticks(0:4:28);
+yticklabels({"2^{-14}", "2^{-10}", "2^{-6}", "2^{-2}", "2^{2}", "2^{6}", "2^{10}", "2^{14}"});
+set(gca, 'YGrid', 'on', 'XGrid', 'off', 'YMinorGrid', 'on', 'XMinorGrid', 'off');
+
+% performance improvements
+figure('Position', [699, 259, 560, 300]);
+bar(results_pcfg1.performance);
+legend(results_pcfg1.legend_cores, "Location", "northwest");
+xticklabels(datasets.names);
+set(gca,'XTickLabelRotation',20);
+xlabel("Data Set");
+ylabel("Speedup");
+grid on;
+set(gca, 'YGrid', 'on', 'XGrid', 'off', 'YMinorGrid', 'on', 'XMinorGrid', 'off');
+
+% performance variation with number of cores
+figure('Position', [699, 259, 280, 250]);
+stem(results_pcfg1.var_accel(1, :), results_pcfg1.var_accel(3, :), "filled");
+ylabel("Execution time [s]");
+xlabel("Number of accelerators");
+grid on;
+set(gca, 'YGrid', 'on', 'XGrid', 'off', 'YMinorGrid', 'off', 'XMinorGrid', 'on');
+grid minor;
+hold on;
+plot(results_pcfg1.var_accel(1, :), results_pcfg1.var_accel(3, 1) ./ results_pcfg1.var_accel(1, :), '--');
+legend("Measured", "Theoretical");
+
+% performance variation with number of cores (small dataset)
+figure('Position', [699, 259, 280, 250]);
+stem(results_pcfg1.var_accel(1, :), results_pcfg1.var_accel(2, :), "filled");
+ylabel("Execution time [s]");
+xlabel("Number of accelerators");
+grid on;
+set(gca, 'YGrid', 'on', 'XGrid', 'off', 'YMinorGrid', 'off', 'XMinorGrid', 'on');
+grid minor;
+hold on;
+plot(results_pcfg1.var_accel(1, :), results_pcfg1.var_accel(2, 1) ./ results_pcfg1.var_accel(1, :), '--');
+legend("Measured", "Theoretical", "Location", "northwest");
+
+% performance variation with number of cores (small dataset)
+% figure('Position', [699, 259, 280, 250]);
+% yyaxis right;
+% stem(results.var_accel(1, :), results.var_accel(2, :), "filled", 'color', [0.85 0.325 0.098]);
+% ylabel("Execution time [s]");
+% hold on;
+% plot(results.var_accel(1, :), results.var_accel(2, 1) ./ results.var_accel(1, :), '--', 'color', [0.85 0.325 0.098]);
+% yyaxis left;
+% stem(results_pcfg1.var_accel(1, :), results_pcfg1.var_accel(2, :), "filled", 'color', [0 0.447 0.741]);
+% ylabel("Execution time [s]");
+% xlabel("Number of accelerators");
+% grid on;
+% set(gca, 'YGrid', 'on', 'XGrid', 'off', 'YMinorGrid', 'off', 'XMinorGrid', 'on');
+% grid minor;
+% hold on;
+% plot(results_pcfg1.var_accel(1, :), results_pcfg1.var_accel(2, 1) ./ results_pcfg1.var_accel(1, :), '--', 'color', [0 0.447 0.741]);
+% L(1) = stem(nan, nan, "filled", 'color', 'black');
+% L(2) = plot(nan, nan, '--', 'color', 'black');
+% legend(L, {'Measured', 'Theoretical'});
+
+% energy improvements
+figure('Position', [699, 259, 280, 310]);
+bar(results_pcfg1.energy(:, end));
+xticklabels(datasets.names);
+set(gca,'XTickLabelRotation',45);
+xlabel("Data Set");
+ylabel("Energy effiency improvement");
+set(gca, 'YGrid', 'on', 'XGrid', 'off', 'YMinorGrid', 'on', 'XMinorGrid', 'off');
+
+% accelerator energy consumption
+figure('Position', [699, 259, 280, 310]);
+bar(log2(accel_pcfg1.energy(:, end) * 8192));
+xticklabels(datasets.names);
+set(gca,'XTickLabelRotation',45);
+xlabel("Data Set");
+ylabel("Energy consumption [J]");
+ax = gca;
+ax.YAxis.MinorTickValues = 1:1:28;
+grid on;
+grid minor;
+ylim([0, 24]);
+yticks(0:4:24);
+yticklabels({"2^{-13}", "2^{-7}", "2^{-5}", "2^{-1}", "2^{3}", "2^{7}", "2^{11}"});
+set(gca, 'YGrid', 'on', 'XGrid', 'off', 'YMinorGrid', 'on', 'XMinorGrid', 'off');
