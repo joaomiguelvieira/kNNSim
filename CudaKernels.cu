de25a0de14fd40ae10bd8e6804f99ab0899fa8f6
João Vieira <joaovieira@odyssey.joaomiguelvieira.com>
2020-11-04 20:52:23 +0000

    Adding kNN CUDA kernels

--- CudaKernels.cu
+++ CudaKernels.cu
@@ -2,6 +2,14 @@ extern "C" {
 #include "CudaKernels.cuh"
 }
 
+/*
+* CUDA-parallelized version of the kNN algorithm.
+* 
+* Each block is responsible for computing the distance of one or more
+* testing samples to all training samples of the dataset. Each thread 
+* within a block calculates the distance between one testing sample
+* and one or more training samples.
+* */
 void cudaKnn(KNNDataset *knnDataset, KNNClassifier *knnClassifier) {
 	// get properties of cuda device 0
 	cudaDeviceProp deviceProp;
@@ -14,10 +22,17 @@ void cudaKnn(KNNDataset *knnDataset, KNNClassifier *knnClassifier) {
 	unsigned long long globalMemMinSize = ((knnDataset->numberTraining + knnDataset->numberTesting) * (knnDataset->numberFeatures + 1)) * sizeof(float);
 	assert(deviceProp.totalGlobalMem > globalMemMinSize);
 
+	// make sure that there is enough shared memory per block to store
+	// the vector of distances
+	unsigned long long minSharedMemPerBlock = knnDataset->numberTraining * sizeof(float);
+	assert(deviceProp.sharedMemPerBlock > minSharedMemPerBlock);
+
 	// number of threads per block is half of maximum number of threads
 	// per block as multiple developers suggest that leads to the best
 	// performance benefits
 	unsigned int threadsPerBlock = deviceProp.maxThreadsPerBlock / 2;
+
+	// number of blocks is set to 
 
 	// prepare gpu launching kernels
 	assert(cudaFree(0) == cudaSuccess);
