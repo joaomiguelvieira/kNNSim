14f5ccbaaa10077d49fd1f0a76820c1605e5627d
João Vieira <joaovieira@odyssey.joaomiguelvieira.com>
2020-11-04 20:26:19 +0000

    Adding kNN CUDA kernels

--- CudaKernels.cu
+++ CudaKernels.cu
@@ -3,6 +3,22 @@ extern "C" {
 }
 
 void cudaKnn(KNNDataset *knnDataset, KNNClassifier *knnClassifier) {
+	// get properties of cuda device 0
+	cudaDeviceProp deviceProp;
+	cudaGetDeviceProperties(&deviceProp, 0);
+
+	// make sure that the device has enough global memory to store the
+	// entire dataset
+	// TODO just make sure that the training set, the results and a
+	// minimal amount of testing samples fit in the gpu global memory
+	unsigned long long globalMemMinSize = ((knnDataset->numberTraining + knnDataset->numberTesting) * (knnDataset->numberFeatures + 1)) * sizeof(float);
+	assert(deviceProp.totalGlobalMem > globalMemMinSize);
+
+	// number of threads per block is half of maximum number of threads
+	// per block as multiple developers suggest that leads to the best
+	// performance benefits
+	unsigned int threadsPerBlock = deviceProp.maxThreadsPerBlock / 2;
+
 	// prepare gpu launching kernels
 	assert(cudaFree(0) == cudaSuccess);
 
